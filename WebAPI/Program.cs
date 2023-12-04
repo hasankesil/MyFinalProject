6b2123e582e21c16e9cbf1c41160c1e81fa238c6
hasankesil <hasankesil@hotmail.com>
2023-12-04 15:42:57 +0300

    Added Autofac integration

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,9 @@
 
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -18,10 +21,12 @@ namespace WebAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<IProductService, ProductManager>();
-            builder.Services.AddSingleton<IProductDal, EfProductDal>();
+            //builder.Services.AddSingleton<IProductService, ProductManager>();
+            //builder.Services.AddSingleton<IProductDal, EfProductDal>();
 
-            
+            builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory()) //Autofac kullanma kodlarý
+               .ConfigureContainer<ContainerBuilder>(builder =>
+               { builder.RegisterModule(new AutofacBusinessModule()); });
 
             var app = builder.Build();
 
